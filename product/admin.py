e660a6b9c00760e9b2244c9ad69f460080513e84
Raneemoqaily7 <raneemoqaily7@gmail.com>
2022-07-16 07:52:57 +0300

    add alternative moodel

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Product , ProductImage,Category
+from .models import Product , ProductImage,Category ,Alternative , Accsesory
 
 # Register your models here.
 admin.site.register (Product)
 admin.site.register (ProductImage)
 admin.site.register (Category)
+admin.site.register (Alternative)
+admin.site.register (Accsesory)
--- product/models.py
+++ product/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+
 class Product (models.Model):
     PRDName = models.CharField(max_length = 100 , verbose_name = _("Product Name"))
     PRDDesc = models.TextField (verbose_name = ("Description"))
-    PRDPrice = models.DecimalField(max_digits = 5 , decimal_places= 3  , verbose_name = _("Price"))
-    PRDCost =models.DecimalField(max_digits = 5 , decimal_places= 3 , verbose_name = _("Cost"))
+    PRDPrice = models.DecimalField(max_digits = 6 , decimal_places= 3  , verbose_name = _("Price"))
+    PRDCost =models.DecimalField(max_digits = 6 , decimal_places= 3 , verbose_name = _("Cost"))
     PRDCreated =models.DateTimeField (verbose_name = _("created at"))
 
 
     def __str__(self):
         return self.PRDName
+
 
 
 
@@ -27,7 +29,7 @@ class ProductImage(models.Model):
 
 
 class Category (models.Model):
-    CATName = models.CharField (max_length = 50 ,verbose_name = ("category"))
+    CATName = models.CharField (max_length = 50 ,verbose_name = _("category"))
     CATParent = models.ForeignKey ('self' ,on_delete = models.CASCADE ,limit_choices_to={'CATParent__isnull': True},blank=True,
     null=True ,verbose_name = ("Main category"))
     CATImage= models.ImageField (upload_to = 'category/')
@@ -36,3 +38,22 @@ class Category (models.Model):
 
     def __str__ (self):
         return self.CATName
+
+
+
+class Alternative(models.Model):
+    ALTProd = models.ForeignKey (Product,on_delete =models.CASCADE,related_name ='ALT_product' ,verbose_name = _("product") )
+    ALTProducts =models.ManyToManyField(Product ,related_name ='Alternative_products' , verbose_name = _("Alternative products") )
+
+    def __str__(self):
+        return str(self.ALTProd)
+
+class Accsesory(models.Model):
+    ACCSProd = models.ForeignKey (Product,on_delete =models.CASCADE, verbose_name = _("product") ,related_name ='ACC_product')
+    ACCSProducts =models.ManyToManyField(Product ,related_name ='Accsesories' , verbose_name = _("Accsesories") )
+
+    def __str__(self):
+        return str(self.ACCSProd)
+
+
+    
